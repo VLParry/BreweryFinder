30646fe7c07015d45f696618a15bc4a4780cba4e
Victoria Parry <vlparry@gmail.com>
2022-06-29 13:32:08 -0600

    adding get breweries function and get random brewery function

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+window.addEventListener('DOMContentLoaded', () => {
+    getBreweries()
+    getRandomBrewery()
+})
+
+
+//fetch breweries returns all the breweries to the page once the page is loaded
+function getBreweries() {
+    const listOfBreweries = document.getElementById('breweries')
+    fetch("https://api.openbrewerydb.org/breweries?per_page=30")
+        .then(resp => resp.json())
+        .then(data => {
+            data.forEach(brewery => {
+                listOfBreweries.innerHTML +=
+                    `<li> <a href="#">* ${brewery.name} - ${brewery.state}</a></li>`
+            })
+
+        })
+}
+//Click a button to get random brewery
+
+
+function getRandomBrewery() {
+    const random = document.getElementById('random')
+    fetch("https://api.openbrewerydb.org/breweries/random")
+        .then(resp => resp.json())
+        .then(data => {
+            data.forEach(brewery => {
+                random.innerHTML +=
+                    `<li>${brewery.name} - ${brewery.state}</li>`
+            })
+
+        })
+}
+
 //Let user input state they want to find breweries in(FORM)
 
 
@@ -20,10 +55,10 @@
 
 
 
-// const fetchBrewery = () => {
-    fetch("https://api.openbrewerydb.org/breweries?per_page=20")
-    .then(resp => resp.json())
-    .then(data => {
-        console.log("data", data)
-    })
+// const fetchBreweries = () => {
+//     fetch("https://api.openbrewerydb.org/breweries?per_page=30")
+//         .then(resp => resp.json())
+//         .then(data => {
+//             console.log("data", data)
+//         })
 // }
