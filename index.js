8c3bde73379087d7593bcc1008a1244e136e1bb3
Victoria Parry <vlparry@gmail.com>
2022-07-03 14:07:48 -0600

    fixed it

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const listOfBreweries = document.getElementById('breweries')
     const form = document.getElementById('search-brewery')
     
+
     //Let user input state they want to find breweries in(FORM)
     function getBreweriesByState(state) {
         fetch(`https://api.openbrewerydb.org/breweries?by_state=${state}&per_page=50`)
@@ -9,16 +10,40 @@ window.addEventListener('DOMContentLoaded', () => {
             .then(data => {
                 data.forEach(brewery => {
                     listOfBreweries.innerHTML +=
-                        `<li> <a href="#" data-id="${brewery.id}">* ${brewery.name} - ${brewery.state}</a></li>`
+                        `<li> <a onclick="displayBrewery(event)" href="#" data-id="${brewery.id}">* ${brewery.name} - ${brewery.state}</a></li>`
                 })
                 console.log("data", data)
             })
     }
     
-    
-    form.addEventListener('submit', () => {
+    const displayBrewery = (event) => {
+        console.log(event.target.dataset.id)
+        const info = document.getElementById('brewery-info')
+        const ul = document.getElementById('breweries')
+        ul.innerHTML = ' '
+        fetch(`https://api.openbrewerydb.org/breweries/${event.target.dataset.id}`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                breweryinfo.innerHTML =
+                    `<h1>${data.name}</h1>
+                </br>
+                    <h4>City:</h4>
+                     <p>${data.city}</p>
+                    <h4>State:</h4>
+                    <p>${data.state}</p>
+                    <h4>Website:</h4>
+                    <p>${data.website_url}</p>
+                    <h4>Phone #: </h4>
+                    <p>${data.phone}</p>`
+            })
+        
+    }
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
         console.log('test')
-        // listOfBreweries.innerHTML = ""
+        listOfBreweries.innerHTML = ""
         const searchInput = document.getElementById('search')
         const searchValue = searchInput.value.toLowerCase().replace(" ", "_")
         getBreweriesByState(searchValue)
@@ -36,29 +61,7 @@ window.addEventListener('DOMContentLoaded', () => {
 //         brewery.addEventListener('click', displayBrewery))
 
 // }
-// const displayBrewery = (event) => {
-//     console.log(event.target.dataset.id)
-//     const info = document.getElementById('brewery-info')
-//     const ul = document.getElementById('breweries')
-//     ul.innerHTML = ' '
-//     fetch(`https://api.openbrewerydb.org/breweries/${event.target.dataset.id}`)
-//         .then(resp => resp.json())
-//         .then(data => {
-//             console.log(data)
-//             breweryinfo.innerHTML =
-//                 `<h1>${data.name}</h1>
-//             </br>
-//                 <h4>City:</h4>
-//                  <p>${data.city}</p>
-//                 <h4>State:</h4>
-//                 <p>${data.state}</p>
-//                 <h4>Website:</h4>
-//                 <p>${data.website_url}</p>
-//                 <h4>Phone #: </h4>
-//                 <p>${data.phone}</p>`
-//         })
-    
-// }
+
 
 
 
